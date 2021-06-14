4d0bdc175310b214ec197d44229f6e984a47cef2
Khosiyat Sabirova <61965641+Khosiyat@users.noreply.github.com>
2021-06-14 18:34:19 +0300

    Create find_num.py

---
+++ find_num.py
@@ -0,0 +1,7 @@
+#1chi savol:  #uzun sonlar ketma-ketligida N-o'rinda turgan raqamni topuvchi dastur tuzing. Masalan : N = 15
+
+myList=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+n=15
+for i in myList:
+    if i==n:
+        print(i)
